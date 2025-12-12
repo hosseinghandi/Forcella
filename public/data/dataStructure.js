6db2d2180cdb2a359b97b465caa9dfb9dfa3a436
hosseinghandi <ghandih22@gmail.com>
2025-12-12 12:53:51 +0100

    Initial commit

---
+++ public/data/dataStructure.js
@@ -0,0 +1,7 @@
+export const Text = {
+    introText: {
+        op1: "Neapolitan pizza is the version of the round pizza typically prepared in the Italian city of Naples.",
+        op2: "and characterised by a soft, thin dough with high edges.",
+        op3: "The tomatoes are traditionally either San Marzano tomatoes or pomodorini del Piennolo del Vesuvio.",
+    }
+};
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className='w-full h-screen flex justify-center items-center'>
+        <p className='text-5xl text-red-500 text-center '> i am all set  </p>
+      </div>
+    </>
+  )
+}
+
+export default App
